f2bcbcfc96ff283af2f4e5a6a4d34573281e7048
BhaveshKumar <bk769221@gmail.com>
2023-05-18 18:23:00 +0530

    Adding limit and page

--- routes/data.route.js
+++ routes/data.route.js
@@ -5,11 +5,27 @@ const dataRouter = express.Router();
 
 dataRouter.get("/", async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
+  const query = req.query;
   try {
-    let data = await DataModel.find();
-    res.send(data);
+    const page = parseInt(req.query.page) - 1 || 0;
+    const limit = parseInt(req.query.limit) || 5;
+
+    const data = await DataModel.find({})
+      .skip(page * limit)
+      .limit(limit);
+
+    const total = await DataModel.countDocuments();
+
+    const response = {
+      error: false,
+      total,
+      page: page + 1,
+      limit,
+      data,
+    };
+    res.send(response);
   } catch (error) {
-    res.status(404).send({ msg: error.message });
+    res.status(500).send({ error: true, message: "Internal Server Error" });
   }
 });
 
